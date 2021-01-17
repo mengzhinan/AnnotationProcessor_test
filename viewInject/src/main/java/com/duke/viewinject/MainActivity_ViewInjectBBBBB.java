0f44c42b28d70b49dbdeb58147f047185b9cfb3e
duke <136239173@qq.com>
2021-01-17 17:06:53 +0800

    init

--- viewInject/src/main/java/com/duke/viewinject/MainActivity_ViewInjectBBBBB.java
+++
@@ -1,7 +0,0 @@
-package com.duke.viewinject;
-
-class MainActivity_ViewInjectBBBBB {
-//  public MainActivity_ViewInject(MainActivity target, View rootView) {
-//    target.myView=Utils.getViewById(rootView,2131231057);
-//  }
-}
